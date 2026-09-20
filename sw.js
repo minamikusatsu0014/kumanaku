11db5dd4259c7681e8c5885820896aaf47bc8322
minamikusatsu0014 <minamikusatsu0014@gmail.com>
2026-09-21 04:45:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 京都くまなくウォーク Service Worker (GitHub Pages用) */
-const CACHE = 'kyoto-kumanaku-v31';
+const CACHE = 'kyoto-kumanaku-v32';
 const BASE = self.registration.scope;
 const APP_SHELL = [
   BASE,
