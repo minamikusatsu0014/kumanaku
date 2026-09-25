588de4e0859c583b8c4cdd1ab7e7bad9ca5cd3b6
minamikusatsu0014 <minamikusatsu0014@gmail.com>
2026-09-26 07:46:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 街道ウォーキング Service Worker v125 */
-var CACHE='kumanaku-v131';
-var TILE='kumanaku-tiles-v131';
+var CACHE='kumanaku-v132';
+var TILE='kumanaku-tiles-v132';
 var TILE_CAP=260;
 var CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 var TILE_HOSTS=['cyberjapandata.gsi.go.jp','tile.openstreetmap.org','maps.gsi.go.jp'];
